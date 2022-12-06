368c052eebfaa16b91137a1084acc665ee378ab5
Justin Ahinon <justiny.ahinon@gmail.com>
2022-12-06 08:27:02 +0200

    Update +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,7 +3,7 @@
 </script>
 
 {#if $page.status === 401}
-	<h1>{$page.error?.message}</h1>
+	<h1>Error {$page.status}: {$page.error?.message}</h1>
 	<p>
 		Go to <a href="/login">login</a> page to login or <a href="/signup">signup</a> a new account.
 	</p>
