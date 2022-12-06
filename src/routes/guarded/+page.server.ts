f7e12ee5e9c6e21bc736470dd669416f82e72e5d
Justin Ahinon <justiny.ahinon@gmail.com>
2022-12-06 08:23:23 +0200

    Implement log out

--- src/routes/guarded/+page.server.ts
+++ src/routes/guarded/+page.server.ts
@@ -1,5 +1,5 @@
-import type { PageServerLoad } from './$types';
-import { redirect, error } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+import { error, redirect } from '@sveltejs/kit';
 
 export const load: PageServerLoad = (event) => {
 	const user = event.locals.user;
@@ -13,4 +13,12 @@ export const load: PageServerLoad = (event) => {
 	return {
 		user
 	};
+};
+
+export const actions: Actions = {
+	logout: async (event) => {
+		event.cookies.delete('AuthorizationToken');
+
+		throw redirect(302, '/login');
+	}
 };
--- src/routes/guarded/+page.svelte
+++ src/routes/guarded/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -10,7 +11,25 @@
 	<title>Guarded page</title>
 </svelte:head>
 
-<h1>Guarded page</h1>
-<p>This page is guarded and will only be accessible to authenticated users.</p>
+<section>
+	<div>
+		<h1>Guarded page</h1>
+		<p>This page is guarded and will only be accessible to authenticated users.</p>
 
-<p>Hello {user.email}</p>
+		<p>Hello {user.email}</p>
+	</div>
+
+	<form method="POST" action="?/logout">
+		<button type="submit" name="logout" value="true">Logout</button>
+	</form>
+</section>
+
+<style>
+	section > * {
+		margin-bottom: 2rem;
+	}
+
+	form button {
+		width: fit-content;
+	}
+</style>
